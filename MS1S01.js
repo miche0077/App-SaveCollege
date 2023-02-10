38a2f669f2af4086bf09656c6c6cb6cea0ae3636
Michelle Vegas <88907873+miche0077@users.noreply.github.com>
2023-02-10 17:49:00 -0300

    Exercicio 01

---
+++ MS1S01.js
@@ -0,0 +1,44 @@
+var student = [
+    {
+    name: "Gabriel",
+    height: 1.56,
+    sex: "masculino",
+    room: "trindade",
+    age: 16,
+    mathematics: 7
+    },
+    {
+    name: "Mariana",
+    height: 1.7,
+    sex: "feminino",
+    room: "trindade",
+    age: 26,
+    mathematics : 8
+    },
+    {
+    name: "Andressa",
+    height: 1.6,
+    sex: "feminino",
+    room: "trindade",
+    age: 24,
+    mathematics: 10
+    },
+    {
+    name: "Xavier",
+    height: 1.69,
+    sex: "masculino",
+    room: "trindade",
+    age: 17,
+    mathematics: 5
+    },
+    {
+    name: "alex",
+    height: 1.75,
+    sex: "masculino",
+    room: "trindade",
+    age: 27,
+    mathematics: 7
+    },
+    ];
+    
+    console.log(student)
